2c717eda1bbe86f2ebe20a474dc211fc87dbee45
Matias Garcia <64979089+OneWhisper31@users.noreply.github.com>
2023-02-09 13:15:22 -0300

    Inicio Logica Store

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GaboPrueba : MonoBehaviour
+public class Store : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
